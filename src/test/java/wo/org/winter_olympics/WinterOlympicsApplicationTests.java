6be674a4b52a2977da623a950e36dbe323a89f1b
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2026-05-16 23:32:21 +0300

    Initial set up

---
+++ src/test/java/wo/org/winter_olympics/WinterOlympicsApplicationTests.java
@@ -0,0 +1,13 @@
+package wo.org.winter_olympics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterOlympicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
