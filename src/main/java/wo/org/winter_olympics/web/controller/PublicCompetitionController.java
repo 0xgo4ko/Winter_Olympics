b902030b16985d21959ddf6ccc77cd49eb3e6c05
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2026-05-17 23:20:33 +0300

    Implement public comps page

--- src/main/java/wo/org/winter_olympics/web/controller/PublicCompetitionController.java
+++ src/main/java/wo/org/winter_olympics/web/controller/PublicCompetitionController.java
@@ -1,13 +1,22 @@
 package wo.org.winter_olympics.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import wo.org.winter_olympics.core.service.CompetitionService;
 
 @Controller
 public class PublicCompetitionController {
 
+    private final CompetitionService competitionService;
+
+    public PublicCompetitionController(CompetitionService competitionService) {
+        this.competitionService = competitionService;
+    }
+
     @GetMapping("/competitions")
-    public String competitions() {
+    public String competitions(Model model) {
+        model.addAttribute("competitions", competitionService.getAllCompetitions());
         return "competitions";
     }
 }
